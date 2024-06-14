457db63d0551944d4f8a23db336f0ded2cc1544f
SoftLoUi <150540766+SoftLoUi@users.noreply.github.com>
2024-06-13 18:34:48 -0600

    Add files via upload

---
+++ DOM.js
@@ -0,0 +1,10 @@
+document.getElementById('demo').innerHTML = 'Nuevo contenido';
+console.log(document.getElementById('demo').innerHTML);
+
+let introElement = document.getElementById("intro");
+console.log(introElement);
+document.getElementById("demo").innerHTML = "¡Hola Mundo!";
+document.getElementById("intro").innerHTML = "Este es un nuevo párrafo";
+
+const h1Element =  document.querySelector("h1");
+        console.log(h1Element);
